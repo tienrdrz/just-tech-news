fe2eeb04f22bc78c2bfbda2c0666f1d888c29875
etienne <etiennerodriguez3@gmail.com>
2022-08-06 17:06:20 -0400

    you are now logged in

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 //create user model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 
