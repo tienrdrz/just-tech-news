d98f7c72b029e31402aad6f654f7d0724d5c1893
etienne <etiennerodriguez3@gmail.com>
2022-08-06 16:44:54 -0400

    hash password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+const bcrypt = require('bcrypt');
+
 //create user model
 class User extends Model {}
 
@@ -47,6 +49,18 @@ User.init(
         }
       },
       {
+        hooks: {
+          // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+        },
+        // set up beforeUpdate lifecycle 'hook' functionality
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
         sequelize,
         timestamps: false,
         freezeTableName: true,
